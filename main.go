738a4bc0024a69dc1abc870a1d39bd10fd0a8cea
hahwul <hahwul@gmail.com>
2021-02-09 00:27:24 +0900

    Added gee pkg

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+
+	model "github.com/hahwul/gee/pkg/model"
+	printing "github.com/hahwul/gee/pkg/printing"
+)
+
 func main() {
+	version := flag.Bool("version", false, "version of gee")
+	flag.Parse()
+	options := model.Options{}
+	_ = options
+	if *version {
+		fmt.Println(printing.VERSION)
+	}
 
 }
---
+++ pkg/gee/gee.go
@@ -0,0 +1,10 @@
+package gee
+
+import (
+	model "github.com/hahwul/gee/pkg/model"
+)
+
+// Gee is running gee
+func Gee(options model.Options) {
+
+}
