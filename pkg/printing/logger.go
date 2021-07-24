d20e0e7cde5267527774dcbd6ecf9f22c9b4e18b
hahwul <hahwul@gmail.com>
2021-07-25 00:55:35 +0900

    Modified logger (error)

--- pkg/printing/logger.go
+++ pkg/printing/logger.go
@@ -7,8 +7,10 @@ import (
 )
 
 // DebugMsg is msg for debug
-func DebugMsg(t string,m interface{}){
-	fmt.Printf("%s%s%s\n",aurora.BrightYellow("[DEBUG]").String(),aurora.BrightBlue("["+t+"] ").String(),m)
+func DebugMsg(t string, m interface{}, debugOption bool) {
+	if debugOption {
+		fmt.Printf("%s%s%s\n", aurora.BrightYellow("[DEBUG]").String(), aurora.BrightBlue("["+t+"] ").String(), m)
+	}
 }
 
 // ErrPanic is panic error
